6cf467728d049bca11b82e5e2aa6165e44c55f8a
sahilmittal <sahil.mittal89@gmail.com>
2014-03-27 13:57:38 +0530

    test

---
+++ ExtensionContent/jsx/Illustrator.jsx
@@ -0,0 +1,12 @@
+$._ext_ILST={
+    run : function() {
+    
+    	/**********  Replace below sample code with your own JSX code  **********/
+        var appName;	    
+	    appName = "Hello Illustrator";	    
+        alert(appName);
+        /************************************************************************/
+        
+        return appName;
+    },
+};
