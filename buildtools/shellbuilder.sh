9404a33af501e9190c21f46be1d3e9297ceec056
Thomas Wrenn <twrenn@creativeworx.com>
2014-07-16 11:11:29 -0400

    new build process scripts to validate version numbers

--- buildtools/shellbuilder.sh
+++ buildtools/shellbuilder.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-rm -f ./builds/TimeTrackerCSExtension.zxp
-rsync -av --exclude='buildtools' --exclude='.git*' '../' './temp/'
-
-read -s -p "Enter Password: " mypassword
- ./ZXPSignCmd -sign ./temp/ ./builds/TimeTrackerCSExtension.zxp ./cert/signingCertificate.p12 $mypassword -tsa https://timestamp.geotrust.com/tsa
-
-rm -r ./temp/
-open ./builds/TimeTrackerCSExtension.zxp
+./testVersionAndIdEqual.sh
+if [[ $? != 100 ]]; then
+	./build.sh
+fi
