5aa94ff07b3463879e857cb744135775f1613d21
Rishabh <rishabh.jain9191@gmail.com>
2014-10-15 10:55:08 +0530

    Bug Fix:Project window hanging for more than 100 projects

--- controller/controller.js
+++ controller/controller.js
@@ -120,7 +120,7 @@ function($rootScope, $scope, $location,$http, Constants,  preloader, debuggerUti
 	$rootScope.userLoggedState=1;
 	$rootScope.checkUpdateFromMenuClick=0;
 	
-	for(i=0;i<100;i++){
+	for(i=0;i<1000;i++){
 		$rootScope.projectProperties.push(new projectNo(i));
 	}	
 	
